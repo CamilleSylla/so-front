650fc3297e06fe8bccc510f5e8af49ff4969a179
Camille Sylla <camillesylla.web@gmail.com>
2023-04-29 01:55:42 +0200

    feat(product-page): ui modifications

--- src/pages/catalogue/[id]/index.tsx
+++ src/pages/catalogue/[id]/index.tsx
@@ -14,7 +14,7 @@ export default function ProductPage({
 }) {
   const { product, price } = data;
   return (
-    <section className="mt-5 py-20 mx-auto max-w-[1366px]">
+    <section className="mt-5 py-20">
       <Presentation product={product} price={price} />
     </section>
   );
@@ -31,45 +31,21 @@ function Presentation({
 
   return (
     <div className="w-full flex gap-8">
-      <div className="w-[515px] flex flex-col gap-3">
-        <div className="w-full h-[650px] relative">
-          <Image
-            fill
-            className=" object-cover"
-            src={product.images[activeImg]}
-            alt={product.images[activeImg]}
-          />
-        </div>
-        <RadioGroup value={activeImg} onChange={setActiveImg}>
-          <RadioGroup.Label className="sr-only">
-            Images Selector
-          </RadioGroup.Label>
-          <div className=" max-w-full overflow-x-auto whitespace-nowrap">
-            {product.images.map((image, index) => (
-              <RadioGroup.Option
-                className={`${
-                  index === product.images.length - 1 ? "" : " mr-3"
-                } inline-block cursor-pointer`}
-                key={image}
-                value={index}
-              >
-                {({ checked }) => (
-                  <div className={`${
-                    checked ? " opacity-40" : ""
-                  } w-20 h-20 relative`}>
-                    <Image
-                    fill
-                    className=" object-cover"
-                    src={image}
-                    alt={product.name}
-                    
-                  />
-                  </div>
-                )}
-              </RadioGroup.Option>
-            ))}
-          </div>
-        </RadioGroup>
+      <div className="w-1/2 grid grid-cols-2 gap-1">
+        {product.images.map((image, index) => {
+          if (index < 4) {
+            return (
+              <div className="w-full h-[650px] relative">
+                <Image
+                  fill
+                  className=" object-cover"
+                  src={image}
+                  alt={product.name}
+                />
+              </div>
+            );
+          }
+        })}
       </div>
       <PresentationContent product={product} price={price} />
     </div>
@@ -84,24 +60,38 @@ function PresentationContent({
   price: Prices;
 }) {
   return (
-    <div className="flex-1 flex flex-col gap-5">
+    <div className="flex-1 flex flex-col gap-5 mr-40">
       <div>
-      <h1>{product.name}</h1>
-      <h2 className=" text-2xl font-medium"> {product.metadata?.category} </h2>
+        <div className="flex justify-between items-end">
+          <h1>{product.name}</h1>
+          <span className=" font-semibold text-2xl">
+            {useCurrency().formatCurrency(price.value, "fr-FR", price.cur)}
+          </span>
+        </div>
+        <h2 className=" text-2xl font-medium">
+          {" "}
+          {product.metadata?.category}{" "}
+        </h2>
       </div>
-      <p className=" w-4/5">{product.description}</p>
       <p>{"Référence Produit".toUpperCase() + " : " + product.id}</p>
-      <h5 className=" font-semibold text-2xl">
-        {useCurrency().formatCurrency(price.value, "fr-FR", price.cur)}
-      </h5>
+      <div>
+      <h3 className="font-semibold text-2xl">Description du produit : </h3>
+      <p className=" w-4/5">{product.description}</p>
+      </div>
       <ProductFavBtn sideSizes={50} productId={product.id} />
     </div>
   );
 }
-export async function getServerSideProps({ params, res }: { params:  { id: string }, res: NextApiResponse }) {
+export async function getServerSideProps({
+  params,
+  res,
+}: {
+  params: { id: string };
+  res: NextApiResponse;
+}) {
   res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=1800, stale-while-revalidate=3540'
+    "Cache-Control",
+    "public, s-maxage=1800, stale-while-revalidate=3540"
   );
   const { id } = params;
   const data = await fetch(
