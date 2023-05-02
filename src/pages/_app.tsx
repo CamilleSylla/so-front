1d5ed4a03252c3edb31c5092ef8f04c46d0f043e
CamilleSylla <65787716+CamilleSylla@users.noreply.github.com>
2023-05-02 23:31:43 +0200

    feat(routing): about page added with ui and animation parallax #8
    * feat(about): content display grid on page
    
    * feat(about-page): about page content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,15 @@ import Nav from "@/components/Nav";
 import { FavProvider } from "@/context/FavContext";
 import { FilterProvider } from "@/context/ProductsFiltersContext";
 import "@/styles/globals.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+  },[])
   return (
     <FavProvider>
       <FilterProvider>
